fd566dc9d69a7d5baea75de6c3b9b0580f6278ce
Hosanna Otchere <hosannaob2@gmail.com>
2025-06-16 13:34:32 -0500

    WebConfig

--- src/main/java/com/example/minijira/MinijiraApplication.java
+++ src/main/java/com/example/minijira/MinijiraApplication.java
@@ -2,8 +2,14 @@ package com.example.minijira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
 public class MinijiraApplication {
 
     public static void main(String[] args) {
